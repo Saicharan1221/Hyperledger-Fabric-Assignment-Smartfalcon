e4b8955ff967e2b0ac297e8d8c4a25109bc4f922
Saicharan <161144308+Saicharan1221@users.noreply.github.com>
2024-10-23 08:13:12 +0530

    Asset.java

---
+++ Asset.java
@@ -0,0 +1,11 @@
+public class Asset {
+    private String DEALERID;
+    private String MSISDN;
+    private String MPIN;
+    private double BALANCE;
+    private String STATUS;
+    private double TRANSAMOUNT;
+    private String TRANSTYPE;
+    private String REMARKS;
+
+    }
